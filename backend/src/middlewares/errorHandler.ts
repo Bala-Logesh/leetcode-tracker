414318908adc1527feeda9ef9171e5eba08011c8
Bala Logesh S <balassmp@gmail.com>
2026-03-19 11:58:22 -0700

    Fixed error handler middleware

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,24 +1,29 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import logger from '../logger/logger'
 
 export class APIError extends Error {
-    statusCode: number
+  statusCode: number
 
-    constructor(message: string, statusCode: number) {
-        super(message)
-        this.statusCode = statusCode
-    }
+  constructor(message: string, statusCode: number, LOG_PREFIX?: string) {
+    super(message)
+    this.statusCode = statusCode
+  }
 }
 
-export const errorHandler = (err: APIError, req: Request, res: Response) => {
-    const status = err.statusCode !== 200 ? err.statusCode : 500
+export const errorHandler = (
+  err: APIError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const status = err.statusCode !== 200 ? err.statusCode : 500
 
-    logger.error(
-        `${status} - ${err.message} - ${req.originalUrl} - ${req.method}`
-    )
+  logger.error(
+    `${req.method.toUpperCase()} ${req.originalUrl} [${status}] - ${err.message}\nError track: ${err.stack}`
+  )
 
-    res.status(status).json({
-        error: err.message,
-        status,
-    })
+  res.status(status).json({
+    error: err.message,
+    status,
+  })
 }
